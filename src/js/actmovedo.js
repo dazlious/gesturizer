65228a8ac875d57d677bec0ecc091a641386f503
Duve, Michael <michael.duve@bild.de>
2015-07-22 01:53:53 +0200

    tap, doubletap and longpress implemented

--- src/js/actmovedo.js
+++ src/js/actmovedo.js
@@ -16,7 +16,15 @@ var actMoveDo = actMoveDo || (function($) {
                         scroll: 0.05
                     }
                 },
+                treshold: {
+                    tap: 200,
+                    doubletap: 200,
+                    longpress: 500
+                },
                 callbacks: {
+                    tap: null,
+                    doubletap: null,
+                    longpress: null,
                     pan: null,
                     flick: null,
                     zoom: null
@@ -43,7 +51,13 @@ var actMoveDo = actMoveDo || (function($) {
 
             this.current = {
                 downEvent: false,
-                start: [0,0]
+                hasMoved: false,
+                multitouch: false,
+                lastAction: null,
+                start: null,
+                timeStart: null,
+                timeEnd: null,
+                timeout: null
             };
 
             this.init();
@@ -56,7 +70,6 @@ var actMoveDo = actMoveDo || (function($) {
         };
 
         ActMoveDo.prototype.bindEvents = function() {
-
 
             // if device is touch
             if (this.settings.isTouchDevice) {
@@ -81,7 +94,13 @@ var actMoveDo = actMoveDo || (function($) {
             this.$container.on(this.settings.eventNames.start.mouse, this.startHandler.bind(this));
             this.$container.on(this.settings.eventNames.move.mouse, this.moveHandler.bind(this));
             this.$container.on(this.settings.eventNames.end.mouse, this.endHandler.bind(this));
+            /* maybe later for fixing leaving the window
+            this.$container.on("mouseleave", (function(e) {
+                this.current.downEvent = false;
+            }).bind(this));
+             */
         };
+
 
         ActMoveDo.prototype.scrollHandler = function(event) {
             event.stopPropagation();
@@ -98,23 +117,48 @@ var actMoveDo = actMoveDo || (function($) {
 
             var e = this.getEvent(event);
 
+            this.current.target = event.target;
             this.current.downEvent = true;
+            this.current.timeStart = event.timeStamp;
+
+            if (this.current.timeout) {
+                this.current.timeout = clearTimeout(this.current.timeout);
+            }
 
             // mouse is used
             if (e instanceof MouseEvent) {
-                console.log("mouse down", e);
+                this.current.start = this.getRelativePosition(e);
             } // touch is used
             else {
                 // singletouch startet
                 if (e.length <= 1) {
-                    console.log("single touch", e);
+                    this.current.start = this.getRelativePosition(e[0]);
                 } // multitouch started
-                else {
-                    console.log("multi touch", e);
+                else if (e.length === 2) {
+                    this.current.multitouch = true;
+                    var pos1 = this.getRelativePosition(e[0]),
+                        pos2 = this.getRelativePosition(e[1]);
 
+                    // using pythagoras calculating center
+                    var a = pos1[0] - pos2[0],
+                        b = pos1[1] - pos2[1];
+
+                    this.current.start = [(pos1[0] + pos2[0]) / 2, (pos1[1] + pos2[1]) / 2];
+                    this.current.distance = Math.sqrt(a * a + b * b);
                 }
-                console.log(e);
             }
+            switch(this.current.lastAction) {
+                case null:
+                    this.current.lastAction = "tap";
+                    break;
+                case "tap":
+                    this.current.lastAction = "doubletap";
+                    break;
+                default:
+                    break;
+            }
+
+            console.log(this.current);
         };
 
         ActMoveDo.prototype.moveHandler = function(event) {
@@ -122,6 +166,8 @@ var actMoveDo = actMoveDo || (function($) {
             if (!this.current.downEvent) {
                 return false;
             }
+
+            this.current.hasMoved = true;
 
             event.stopPropagation();
             event.preventDefault();
@@ -133,14 +179,61 @@ var actMoveDo = actMoveDo || (function($) {
 
         ActMoveDo.prototype.endHandler = function(event) {
 
-            this.current.downEvent = false;
-
             event.stopPropagation();
             event.preventDefault();
 
             var e = this.getEvent(event);
 
-            console.log("end", e);
+            this.current.timeEnd = event.timeStamp;
+
+            var timeDiff = this.current.timeEnd - this.current.timeStart;
+
+            if (!this.current.hasMoved && this.current.downEvent && !this.current.multitouch) {
+                switch(this.current.lastAction) {
+                    case "tap":
+                        if (timeDiff < this.settings.treshold.longpress) {
+                            this.setTimeoutForEvent(this.settings.callbacks.tap, this.settings.treshold.tap, this.current.target, this.current.start);
+                        } else {
+                            this.eventCallback(this.settings.callbacks.longpress, [this.current.target, this.current.start]);
+                        }
+                        break;
+                    case "doubletap":
+                        this.setTimeoutForEvent(this.settings.callbacks.doubletap, this.settings.treshold.doubletap, this.current.target, this.current.start);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            this.current.downEvent = false;
+            this.current.hasMoved = false;
+            this.current.multitouch = false;
+        };
+
+        ActMoveDo.prototype.setTimeoutForEvent = function(callback, timeout) {
+            var self = this;
+
+            var args = [];
+            for (var i = 2; i < arguments.length; i++) {
+                args[i-2] = arguments[i];
+            }
+
+            this.current.timeout = setTimeout(this.eventCallback.bind(this, callback, args), timeout);
+        };
+
+        ActMoveDo.prototype.eventCallback = function(callback, args) {
+            if (callback) {
+                callback(args);
+            }
+            this.current.lastAction = null;
+        };
+
+        ActMoveDo.prototype.getRelativePosition = function(e) {
+            var target = e.target || e.srcElement,
+                clientBounds = target.getBoundingClientRect(),
+                x = (e.clientX - clientBounds.left) / clientBounds.width,
+                y = (e.clientY - clientBounds.top) / clientBounds.height;
+            return [x, y];
         };
 
         ActMoveDo.prototype.getScrollDirection = function(event) {
@@ -177,6 +270,7 @@ var actMoveDo = actMoveDo || (function($) {
         };
 
         ActMoveDo.prototype.getEvent = function(e) {
+            jQuery.event.fix(e);
             return e.originalEvent.touches || e.originalEvent.changedTouches || e.originalEvent;
         };
 
